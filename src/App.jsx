332150a1169576a6c39078072e40ea0f247183f9
Iryna Polchaninova <ira.polchanin@gmail.com>
2024-07-26 16:06:57 +0300

    added random-color project

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,13 @@
-import { useState } from "react";
-import data from './data';
+import Accordion from "./components/accordion/accordion"
+import RandomColorGen from "./components/random-color/RandomColorGen"
 
 function App() {
-  const [selected, setSelected] = useState(null);
-  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
-  const [multiple, setMultiple] = useState([]);
-
-  const handleSingleSelection = (getCurrentId) => {
-    setSelected(getCurrentId === selected ? null : getCurrentId)
-  }
-
-  const handleMultipleSelection = (getCurrentId) => {
-    const copyMultiple = [...multiple];
-    const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
-    if (findIndexOfCurrentId === -1) copyMultiple.push(getCurrentId)
-    else copyMultiple.splice(findIndexOfCurrentId, 1);
-    setMultiple(copyMultiple)
-    console.log(multiple)
-  }
 
   return (
-    <div className="acc-wrapper">
-      <button
-        onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
-        {enableMultiSelection ? "Multi" : "Single"} Selection
-      </button>
-
-      <div className="accordion">
-        {data && data.length > 0
-          ? data.map(dataItem => (
-            <div className="item" key={dataItem.id}>
-              <div
-                className="title"
-                onClick={() => enableMultiSelection
-                  ? handleMultipleSelection(dataItem.id)
-                  : handleSingleSelection(dataItem.id)
-                }
-              >
-                <h3>{dataItem.question}</h3>
-                <span>+</span>
-              </div>
-              {selected === dataItem.id || (enableMultiSelection && multiple.indexOf(dataItem.id) !== -1)
-                ? <div className="content">{dataItem.answer}</div>
-                : null}
-            </div>
-          ))
-          : <div>No data found</div>
-        }
-      </div>
-    </div>
+    <>
+      {/* <Accordion /> */}
+      <RandomColorGen />
+    </>
   )
 }
 
---
+++ src/components/random-color/RandomColorGen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RandomColorGen = () => {
+  return (
+    <div className='container'>
+      <button>
+        Generate Random Color
+      </button>
+    </div>
+  )
+}
+
+export default RandomColorGen
